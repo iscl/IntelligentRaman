fcd1af51216b7bc90e7cdc90751466bfae4996e9
Robin <xiajiabinxjb@gmail.com>
2018-12-03 16:52:59 +0800

    评价机制

--- test.py
+++ test.py
@@ -1,82 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, roc_curve
 # from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
+# 数据录入_训练集#
 data_train = pd.read_csv(r'F:\iscl\IntelligentRaman\data\train.csv.bz2', index_col=False)
 y_train = data_train[['molecule', 'concentration']]
 y_train = y_train['molecule'].values
 x_train = data_train.drop(['molecule', 'concentration'], axis=1)
 spectra = x_train['spectra'].values
 x_train = np.array([np.array(dd[1:-1].split(',')).astype(float) for dd in spectra])
-# X_train['spectra'] = spectra.tolist()
 print('x_train.shape:', x_train.shape)
 
 
+# 数据录入_测试集#
 data_test = pd.read_csv(r'F:\iscl\IntelligentRaman\data\test.csv.bz2', index_col=False)
 y_test = data_test[['molecule', 'concentration']]
 y_test = y_test['molecule'].values
 x_test = data_test.drop(['molecule', 'concentration'], axis=1)
 spectra = x_test['spectra'].values
 x_test = np.array([np.array(dd[1:-1].split(',')).astype(float) for dd in spectra])
-# x_test['spectra'] = spectra.tolist()
 print('x_test.shape:', x_test.shape)
-#
-# train_accuracy = []
-# test_accuracy = []
-# n = []
-# for n_comp in range(1, 200):
-#     n.append(n_comp)
-#     print(n_comp)
-#     pca = PCA(n_components=n_comp)
-#     pca_train = pca.fit_transform(x_train)
-#     pca_test = pca.fit_transform(x_test)
-#
-#     lda = LDA(n_components=8)
-#     lda_train = lda.fit(pca_train, y_train)
-#     lda_train_accuracy = lda.score(pca_train, y_train)
-#     train_accuracy.append(lda_train_accuracy)
-#
-#     lda_test_accuracy = lda.score(pca_test, y_test)
-#     test_accuracy.append(lda_test_accuracy)
 
-
+# PCA降维#
 pca_n_comp = 30
 pca = PCA(n_components=pca_n_comp)
 pca_train = pca.fit_transform(x_train)
 pca_test = pca.fit_transform(x_test)
 
+
+# LDA分类#
 lda = LDA(n_components=4)
 lda_train = lda.fit(pca_train, y_train)
 print(lda_train)
 lda_train_accuracy = lda.score(pca_train, y_train)
 lda_test_accuracy = lda.score(pca_test, y_test)
-
 print('lda_train_accuracy:', lda_train_accuracy)
 print('lda_test_accuracy:', lda_test_accuracy)
 
-svm = SVC(C=1e5, gamma=0.01)
+
+# SVM分类#
+svm = SVC(C=1e5, gamma=0.01, probability=True)
 svm_train = svm.fit(pca_train, y_train)
 print(svm_train)
 svm_train_accuracy = svm.score(pca_test, y_test)
 svm_test_accuracy = svm.score(pca_test, y_test)
-
 print('svm_train_accuracy:', svm_train_accuracy)
 print('svm_test_accuracy:', svm_test_accuracy)
+svm_pred = svm.predict(pca_test)
+svm_prpbas = svm.predict_proba(pca_test)
+print(svm_prpbas.shape)
+print(y_test.shape)
 
 
+# 评价部分_混淆矩阵#
+cm = confusion_matrix(y_test, svm_pred, labels=None, sample_weight=None)
+print(cm)
 
 
+# 评价部分_混淆矩阵#
+precision = precision_score(y_test, svm_pred, average=None)
+recall = recall_score(y_test, svm_pred, average=None)
+F1 = f1_score(y_test, svm_pred, average=None)
+fpr, tpr, thresholds = roc_curve(y_test, svm_prpbas[:, 1], pos_label='A')
+tnr = 1-fpr
+print(tnr.shape)
+print('precision_score:', precision, '\n', 'recall_score:', recall, '\n',
+      'recall_score:', F1, '\n', 'Sensitivity:', fpr, '\n', 'Specificity:', tnr)
 
 
-# plt.plot(n, test_accuracy, linewidth=2,
-#          label="LDA with shrinkage", color='r')
-# plt.plot(n, train_accuracy, linewidth=2,
-#          label="LDA with shrinkage", color='g')
-# plt.xlabel('n_components')
-# plt.ylabel('Classification accuracy')
-# plt.show()
+# 绘图
+plt.matshow(cm, cmap=plt.cm.Reds)
+plt.colorbar()
+for x in range(len(cm)):
+    for y in range(len(cm)):
+        plt.annotate(cm[x, y], xy=(x, y), horizontalalignment='center', verticalalignment='center')
+plt.show()
